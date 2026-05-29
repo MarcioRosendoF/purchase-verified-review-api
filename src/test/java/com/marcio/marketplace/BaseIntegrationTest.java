cfe7e76b564b9731eb29f3425c9bccb4c8974f79
MarcioRosendoF <145620413+MarcioRosendoF@users.noreply.github.com>
2026-05-29 16:55:44 -0300

    fix(test): implement singleton container pattern for testcontainers

--- src/test/java/com/marcio/marketplace/BaseIntegrationTest.java
+++ src/test/java/com/marcio/marketplace/BaseIntegrationTest.java
@@ -5,17 +5,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@Testcontainers
 public abstract class BaseIntegrationTest {
 
-    @Container
     @ServiceConnection
     protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    static {
+        postgres.start();
+    }
 
 }
